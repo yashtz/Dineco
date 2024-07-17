c0eaa678c371cd0a6e65053d3003253f8236815b
yashtz <yashpatil1848@gmail.com>
2024-07-17 22:21:41 +0530

    install react icons and fix tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,16 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Account from './pages/MyProfile';
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
