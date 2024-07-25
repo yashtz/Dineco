addca617a379c74dd5b7e15a221b7fb34aafcc52
Yash Patil <yashpatil1848@gmail.com>
2024-07-26 02:58:41 +0530

    Add home page and home route on app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
 function App() {  
   return (
     <Router>
       <div>
         <Navbar />
-        {/* <Footer /> */}
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+        </Routes>
       </div>
     </Router>
   );
 }
 
 export default App;
-
